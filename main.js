d17767e7f527d5acb9459c37ec8db62e974b7204
Preeti12345678 <75837980+Preeti12345678@users.noreply.github.com>
2021-07-18 08:16:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -23,11 +23,18 @@ var ball = {
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent("canvas");
+  video=createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+  posenet=ml5.poseNet(video,modelLoaded);
+}
+function modelLoaded(){
+  console.log("Model Loaded");
 }
 
-
 function draw(){
-
+image(video,0,0,700,600);
  background(0); 
 
  fill("black");
